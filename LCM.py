3fc316c46f46bac32af8df4bfd6acc8b097850cd
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-07-19 17:49:51 +0530

    Add LCM calculation script in LCM.py
    tried to write a code to find lCM by using traditional method instead of using formula lcm = a * b // gcd (a, b)

---
+++ LCM.py
@@ -0,0 +1,38 @@
+num_one = int(input("enter first number : "))
+num_two = int(input("enter second number : "))
+
+greater_num = None
+if num_one > num_two:
+    greater_num = num_one
+else:
+    greater_num = num_two
+
+lcm = 1 
+
+if num_one == 0 or num_two == 0:
+    print("enter both number less then or greather then  0 ")
+else:
+    for i in range(2, greater_num + 1):
+
+        while num_one % i == 0 or num_two % i == 0 :
+        
+            if num_one % i == 0 :
+                num_one = num_one // i
+
+            if num_two % i == 0 :
+                num_two = num_two // i 
+
+            lcm = lcm * i
+
+
+    print(lcm)
+
+
+    
+
+
+
+
+
+
+
