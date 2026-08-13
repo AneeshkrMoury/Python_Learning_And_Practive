47df6ec0a1a89be87537ec5c9f9ef808cdeef582
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-08-13 15:26:32 +0530

    Correct typos and enhance comments in code
    Fixed typos and improved comments in the mini food delivery app.

--- smallproject/mini_food_delivery_app.py
+++ smallproject/mini_food_delivery_app.py
@@ -1,6 +1,8 @@
-# Mini Food delivery app
+# Mini Food delivery app 
+# a restaurant has a menu of dishes . a custome builds an ordr by adding dishes and we show the running total
 
-# a restaurant has a menu of dishes . a customer builds an order by adding dishes and we show the running total
+
+
 
 '''
 Noun in the problem        Big enough to be a class?       Becomes
@@ -20,38 +22,58 @@ class MenuItem:
         self.name = name
         self.price = price
 
-class Restaurant:
+    def __repr__(self):  # repare method to correct priniting problem of order list itmes present in order items
+        return f"{self.name} (${self.price})"
+
+
+
+class Restorant:
     def __init__(self, name):
-        self.name = name
-        self.menu = []
+        self.anme = name
+        self.menu = []   
 
     def add_dish(self, item):
         self.menu.append(item)
 
     def show_menu(self):
         for item in self.menu:
-            print(f"{item.name} - ${item.price}")
+            print(f"{item.name} -${item.price}")
 
-class Order:
+
+class order:
     def __init__(self, customer):
         self.customer = customer
         self.items = []
 
-    def add_item(self, item):
+    def add_item(self , item):
         self.items.append(item)
         print(f"Added {item.name}")
 
     def total(self):
         return sum(item.price for item in self.items)
+    
+    def remove_item(self, item):
+        self.items.remove(item)
+        print(f"{item} removed from order of {self.customer}")
 
-pizza = MenuItem("Pizz", 250)
+
+pizza = MenuItem("Pizz", 250)  # add menuitems
 coke = MenuItem("Coke", 60)
 
-r = Restaurant("PizzHut")
-r.add_dish(pizza)
+r = Restorant("PizzHut")   #add restorant 
+r.add_dish(pizza) # add items 
 r.add_dish(coke)
 
-a = order("Aneesh")
-a.add_item(r.menu[0])
+
+a = order("Aneesh") # customer 
+
+r.show_menu() # check menu 
+
+a.add_item(r.menu[0])  # add item 
 a.add_item(r.menu[1])
-print(f"Total: ${a.total()}")
+
+print(f"Total: ${a.total()}") # check total
+
+a.remove_item(a.items[1])
+
+print(a.items)
