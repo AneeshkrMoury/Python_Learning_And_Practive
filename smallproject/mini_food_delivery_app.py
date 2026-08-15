94e9ca89af685074dae1afa9b0e55cbd19afb1fe
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-08-15 15:21:35 +0530

    Fix variable names and enhance order functionality
    Corrected variable names, added delivery fee and discount functionality, and improved bill display.

--- smallproject/mini_food_delivery_app.py
+++ smallproject/mini_food_delivery_app.py
@@ -29,7 +29,7 @@ class MenuItem:
 
 class Restorant:
     def __init__(self, name):
-        self.anme = name
+        self.nameme = name
         self.menu = []   
 
     def add_dish(self, item):
@@ -44,36 +44,75 @@ class order:
     def __init__(self, customer):
         self.customer = customer
         self.items = []
+        self.discounts = 0
+        self.dilivery_fess = 25
 
     def add_item(self , item):
         self.items.append(item)
         print(f"Added {item.name}")
 
-    def total(self):
+    def Sub_total(self):
         return sum(item.price for item in self.items)
     
     def remove_item(self, item):
         self.items.remove(item)
         print(f"{item} removed from order of {self.customer}")
 
+    def show_bill(self):
+        self.item_width = 25
+        self.price_witdth = 12
+        self.total_with = 37
 
-pizza = MenuItem("Pizz", 250)  # add menuitems
+        print(f"="*self.total_with)
+        print("INVOICE / RECEPIT".center(self.total_with, " "))
+
+        for item in self.items:
+            dish_name = (item.name).ljust(self.item_width)
+            dish_price = str(item.price).rjust(self.price_witdth)
+            print(dish_name, dish_price)
+
+        dis_price = self.Sub_total() * self.discounts / 100
+
+        print(f"-"*self.total_with)
+
+        print ("Sub Total".ljust(self.item_width), str(self.Sub_total()).rjust(self.price_witdth))
+        print(f"Discount ( {self.discounts}% )".ljust(self.item_width), str(dis_price).rjust(self.price_witdth))
+        print("Dilivery Charge".ljust(self.item_width), str(self.dilivery_fess).rjust(self.price_witdth))
+        print("Final Total".ljust(self.item_width), str(self.Sub_total() - dis_price + self.dilivery_fess).rjust(self.price_witdth))
+        print(f"-"*self.total_with)
+
+    # M3. Extend the food app: give Order a delivery fee and a apply_discount(percent)  method. Add a second restaurant and show orders can be built from either.
+    def apply_discount(self, discount):
+        self.discounts = discount
+
+
+
+pizza = MenuItem("Pizz", 250)
 coke = MenuItem("Coke", 60)
-
-r = Restorant("PizzHut")   #add restorant 
-r.add_dish(pizza) # add items 
+pasta = MenuItem("Pata", 80)
+r = Restorant("PizzHut")
+r.add_dish(pizza)
 r.add_dish(coke)
-
 
 a = order("Aneesh") # customer 
 
-r.show_menu() # check menu 
-
+# r.show_menu() # check menu 
 a.add_item(r.menu[0])  # add item 
 a.add_item(r.menu[1])
+a.apply_discount(25)
+# print(f"Total: ${a.Sub_total()}") # check total
+# a.remove_item(a.items[1])
+a.show_bill()
 
-print(f"Total: ${a.total()}") # check total
+r2 = Restorant("ItalioHut")
+r2.add_dish(pizza)
+r2.add_dish(coke)
+r2.add_dish(pasta)
 
-a.remove_item(a.items[1])
+b = order("Ravi")
+b.add_item(r2.menu[0])
+b.add_item(r2.menu[1])
+b.add_item(r2.menu[2])
 
-print(a.items)
+b.show_bill()
+
