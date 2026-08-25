d0bd9e6159f4647473415667bef2282f7bf5dbf3
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-08-25 13:24:34 +0530

    Refactor learning.py for clarity and examples
    Updated comments for clarity and corrected indentation in the for loop. Added example of custom iterator with CountupTo class.

--- Phase_2/itrables&itrator/learning.py
+++ Phase_2/itrables&itrator/learning.py
@@ -1,6 +1,6 @@
 nums = [10,20,30,40,50]
-# for i in nums:   # how it works -> at backgrount oython create a iterator named x 
-#     print(i)
+for i in nums:   # how it works -> at backgrount oython create a iterator named x 
+    print(i)
 
 
 # somethink like this happens in background when we do a loop
@@ -18,7 +18,7 @@ in this we get iterables and terators
 
 # itrables 
 nums = [10,20,30,40,50]  #on item on which loops work are itrables 
-# print(nums)
+print(nums)
 
 #itrator
 
@@ -33,3 +33,36 @@ print(next(it))
 
 #iterables  have a dunder method -> __iter__
 # iterator have 2 dunder method -> __iter__, __next__
+
+
+l1 = [10,20,30,40] #-> list is itrables 
+# coverting l1 in iterator
+it = iter(l1) #-> l1 is now coverted to iterator
+print(next(it)) #-> we use next name pointer to access iterator
+print(next(it))
+print(next(it))
+print(next(it))
+
+
+#Builing Custome iterators 
+
+#iterator() - iter(obj)--> __iter__ , next(object) --> __next__ 
+
+class CountupTo:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 1 # track where we are 
+
+    def __iter__(self):
+        return self # object is its on iterator iter() - __iter__, 
+
+    def __next__(self):
+        if self.current > self.limit:
+            raise StopIteration # no more items -> signal done
+        value = self.current
+        self.current = self.current + 1
+        return value
+
+obj = CountupTo(5)
+for n in obj: # calls iter() --->it = iter(obj), call next(it)
+    print(n) 
