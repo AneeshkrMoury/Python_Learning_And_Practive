f42dc68ecb75e1937288836ceffdeb5a7c4ab6f3
Aneesh Kumar Mourya <aneeshkrmourya@gmail.com>
2026-08-26 14:14:33 +0530

    Implement optimized longest consecutive sequence function
    Added an optimized solution for finding the longest consecutive sequence with O(n) time complexity.

--- randomproblem/Longest_Consecutive_Sequence.py
+++ randomproblem/Longest_Consecutive_Sequence.py
@@ -27,3 +27,31 @@ def longest_sequence(data):
     return seq_len
 
 print(longest_sequence(l))
+
+#second solution 
+'''
+Optimized version with O(n) time complexcity
+'''
+l = [100,200,2,3,4,41,42,43,44]
+
+def longest_sequence(data):
+    #covert the data into set 
+    set_data = set(data)
+    seq_len = 0       
+    for i in set_data:
+        # check if i-1 present in data if yes move one to next element in data 
+        if i - 1 in set_data:
+            continue
+        else:
+            counter = i
+            current_len = 1
+            while counter+1 in set_data:
+                counter = counter+1
+                current_len = current_len+1
+
+            if current_len > seq_len:
+                seq_len = current_len
+
+    return seq_len
+
+print(longest_sequence(l))  
